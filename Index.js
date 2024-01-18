3805f5fa84bc659794480255c76db91e47ae513e
DiegoCortez04 <nutritivesquid@gmail.com>
2024-01-18 09:58:22 -0500

    ejercicio4

--- Index.js
+++ Index.js
@@ -3,7 +3,7 @@ const app = express();
 
 //Rutas (Request,Response)
 app.get('/',function(req,res){
-    res.send('Mi primera ruta funciona 25AM')
+    res.send('Inicializo correctamente')
 });
 
 app.get('/alumnos',function(req,res){
@@ -16,4 +16,38 @@ app.get('/alumnos',function(req,res){
 const port = 3000;
 app.listen(port,()=>{
     console.log("El servidor escucha en el puerto " + port)
-})
+});
+
+app.get('/Get',function(req,res){
+    res.send('Servidor Corriendo')
+});
+
+app.post('/Post',function(req,res){
+    res.send('Servidor Corriendo')
+});
+
+app.post("/Alumno",function(req,res){
+    res.json(
+        {"nombre":"Jorge","Apellido":"De los Santos", "Matricula":"202200451", "Carrera":"Ing.Software"}
+    );
+});
+
+app.post("/Producto",function(req,res){
+    res.json([
+        {"nombreproducto":"Shampoo","cantidad":"10","Precio":"80.00"},
+        {"nombreproducto":"Sal","cantidad":"100","Precio":"50.00"},
+        {"nombreproducto":"Papas","cantidad":"1","Precio":"20.00"},
+    ]
+    );
+});
+
+app.post("/Metodos",function(req,res){
+    res.json([
+        {"nombre":"Get","Descripcion":"Solicita una representacion de un recurso especifico"},
+        {"nombre":"Post","Descripcion":"Se utiliza para enviar una entidad o un recurso especifico"},
+        {"nombre":"Put","Descripcion":"Es utilizado para aplicar modificaciones parciales a un recurso"},
+        {"nombre":"Delete","Descripcion":"Borra un recurso especifico"},
+    ]
+    );
+});
+
